a2d8f57a6976085ec9bf491182356b63fc1497b1
kongjingchun <jing20130808@vip.qq.com>
2021-02-23 18:44:35 +0800

    添加九九乘法表

---
+++ test/nine_nine.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# @Create time: 2021/2/23 6:42 下午
+# @Author: KongJingchun
+# @remark: 九九乘法表
+
+for i in range(1, 10):
+    j = 1
+    while j < i + 1:
+        print('{} * {} = {}'.format(j, i, i * j), end=' ')
+        j += 1
+    print()
