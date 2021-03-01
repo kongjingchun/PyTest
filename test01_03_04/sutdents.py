1df4491afd1af39721da533c6144159d876af7bb
kongjingchun <jing20130808@vip.qq.com>
2021-03-01 11:25:05 +0800

    完成学生库

--- test01_03_04/sutdents.py
+++ test01_03_04/sutdents.py
@@ -37,6 +37,18 @@ students = {
 }
 
 
+def check(**kwargs):
+    if 'name' not in kwargs:
+        return '请输入正确的姓名'
+    if 'age' not in kwargs:
+        return '请输入正确的年龄'
+    if 'sex' not in kwargs:
+        return '请输入正确的性别'
+    if 'class_name' not in kwargs:
+        return '请输入正确的工作'
+    return True
+
+
 def get_all_stasuts():
     for key, value in students.items():
         print(
@@ -46,32 +58,46 @@ def get_all_stasuts():
 def del_studen(id):
     if id in students:
         user = students.pop(id)
-        print(f'已删除学号为{id}的学生，姓名：{user["name"]}、年龄：{user["age"]}、性别：{user["sex"]}、工作：{user["class_name"]}')
+        print('已删除学号为{}的学生，姓名：{}、年龄：{}、性别：{}、工作：{}'.format(id, user["name"], user["age"], user["sex"],
+                                                           user["class_name"]))
     else:
         print('不存在该学生')
 
 
 def add_student(**kwargs):
-    if 'name' not in kwargs:
-        print('没有对应的姓名')
+    message = check(**kwargs)
+    if message != True:
+        print(message)
+    else:
+        id = max(students) + 1
+        students[id] = {
+            'name': kwargs['name'],
+            'age': kwargs['age'],
+            'sex': kwargs['sex'],
+            'class_name': kwargs['class_name']
+        }
+        get_all_stasuts()
+
+
+def update_student(**kwargs):
+    if kwargs['id'] not in students:
+        print('请输入正确的id')
         return
-    if 'age' not in kwargs:
-        print('没有对应的年龄')
-        return
-    if 'sex' not in kwargs:
-        print('没有对应的性别')
-        return
-    if 'class_name' not in kwargs:
-        print('没有对应的工作')
-        return
-    id = max(students) + 1
-    students[id] = {
-        'name': kwargs['name'],
-        'age': kwargs['age'],
-        'sex': kwargs['sex'],
-        'class_name': kwargs['class_name']
-    }
+    message = check(**kwargs)
+    if message != True:
+        print(message)
+    else:
+        students[kwargs['id']] = {
+            'name': kwargs['name'],
+            'age': kwargs['age'],
+            'sex': kwargs['sex'],
+            'class_name': kwargs['class_name']
+        }
+        print('已更改学生信息为：姓名：{}、年龄：{}、性别：{}、工作：{}'.format(kwargs['id'], kwargs['name'], kwargs['age'], kwargs['sex'],
+                                                        kwargs['class_name']))
 
 
 get_all_stasuts()
 del_studen(1)
+add_student(name='孔敬淳', age=30, sex='boy')
+update_student(id=4, name='jiaoyuwei', age=29, sex='boy', class_name='开发')
