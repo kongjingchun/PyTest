c01068cf702cf87d0bdc8a44af3b04eef0da0a7d
kongjingchun <jing20130808@vip.qq.com>
2021-03-05 15:16:36 +0800

    装饰器完成

---
+++ test01_04_01/object_decorator2.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+# @Create time: 2021/3/1 4:10 下午
+# @Author: KongJingchun
+# @remark: 装饰器
+
+class Test(object):
+    def __init__(self, name):
+        self._name = name
+
+    def run(self):
+        print('run')
+        self.dump()
+
+    @classmethod
+    def dump(cls):
+        print('dump')
+
+    @staticmethod
+    def sleep():
+        print( 'sleep')
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,value):
+        self._name = value
+
+
+a = Test('kjc')
+a.run()
+Test.sleep()
+print(a.name)
+a.name = 'kkk'
+print(a.name)
+
