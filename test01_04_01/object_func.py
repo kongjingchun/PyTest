6f6374b0fa9b0492b353202ae6994567adc60703
kongjingchun <jing20130808@vip.qq.com>
2021-03-11 19:51:02 +0800

    简单提交

---
+++ test01_04_01/object_func.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+# @Create time: 2021/3/8 6:09 下午
+# @Author: KongJingchun
+# @remark: 类的高级函数 __str__ 和 __getattr__
+
+class Test(object):
+    def __str__(self):
+        return '这个是方法的描述'
+
+    def __getattr__(self, item):
+        return '没有这个key:{}，并不存在！'.format(item)
+
+
+t = Test()
+print(t)
+print(t.a)
