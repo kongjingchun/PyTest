d6848e244b3a323f7d7bdd141940b5d29f5d2f30
kongjingchun <jing20130808@vip.qq.com>
2021-03-11 21:55:40 +0800

    练习面向函数转向面向对象

--- test01_04_01/object_func.py
+++ test01_04_01/object_func.py
@@ -10,7 +10,15 @@ class Test(object):
     def __getattr__(self, item):
         return '没有这个key:{}，并不存在！'.format(item)
 
+    def __setattr__(self, key, value):
+        print(key, value)
+
+    def __call__(self, a):
+        print(a)
+
 
 t = Test()
 print(t)
 print(t.a)
+t.name = 'kjc'
+t('nihao')
---
+++ test01_04_01/students.py
@@ -0,0 +1,106 @@
+# coding:utf-8
+# @Create time: 2021/2/25 4:51 下午
+# @Author: KongJingchun
+# @remark:实现学生信息库
+
+class StudentInfo(object):
+    def __init__(self, students):
+        self.students = students
+
+    def get_all(self):
+        for key, value in self.students.items():
+            print(
+                '学号：{}、姓名：{}、年龄：{}、性别：{}、工作：{}'.format(key, value['name'], value['age'], value['sex'],
+                                                       value['class_name']))
+
+    def check(self, **kwargs):
+        if 'name' not in kwargs:
+            return '请输入正确的姓名'
+        if 'age' not in kwargs:
+            return '请输入正确的年龄'
+        if 'sex' not in kwargs:
+            return '请输入正确的性别'
+        if 'class_name' not in kwargs:
+            return '请输入正确的工作'
+        return True
+
+    def delete(self, id):
+        if id in self.students:
+            user = self.students.pop(id)
+            print('已删除学号为{}的学生，姓名：{}、年龄：{}、性别：{}、工作：{}'.format(id, user["name"], user["age"], user["sex"],
+                                                               user["class_name"]))
+        else:
+            print('不存在该学生')
+
+    def add_student(self, **kwargs):
+        message = self.check(**kwargs)
+        if message != True:
+            print(message)
+        else:
+            id = max(self.students) + 1
+            self.students[id] = {
+                'name': kwargs['name'],
+                'age': kwargs['age'],
+                'sex': kwargs['sex'],
+                'class_name': kwargs['class_name']
+            }
+            self.get_all()
+
+    def update(self, **kwargs):
+        if kwargs['id'] not in self.students:
+            print('请输入正确的id')
+            return
+        message = self.check(**kwargs)
+        if message != True:
+            print(message)
+        else:
+            self.students[kwargs['id']] = {
+                'name': kwargs['name'],
+                'age': kwargs['age'],
+                'sex': kwargs['sex'],
+                'class_name': kwargs['class_name']
+            }
+            print('已更改学生信息为：学号：{}、姓名：{}、年龄：{}、性别：{}、工作：{}'.format(kwargs['id'], kwargs['name'], kwargs['age'],
+                                                                  kwargs['sex'],
+                                                                  kwargs['class_name']))
+
+
+if __name__ == '__main__':
+    students = {
+        1: {
+            'name': '孔敬淳',
+            'age': 30,
+            'sex': "boy",
+            'class_name': '测试'
+        },
+        2: {
+            'name': '王  野',
+            'age': 25,
+            'sex': 'boy',
+            'class_name': "测试"
+        },
+        3: {
+            'name': '卢晓倩',
+            'age': 18,
+            'sex': 'girl',
+            'class_name': '测试'
+        },
+        4: {
+            'name': '焦宇威',
+            'age': 29,
+            'sex': 'boy',
+            'class_name': '开发'
+        },
+        5: {
+            'name': '刘  丹',
+            'age': 20,
+            'sex': 'girl',
+            'class_name': '开发'
+        }
+    }
+
+    student_info = StudentInfo(students)
+    student_info.get_all()
+    student_info.add_student(name='曹雅杰', age=27, sex='girl', class_name='家庭主妇')
+    student_info.delete(6)
+    student_info.update(id=1, name='kjc', age=27, sex='boy', class_name='开发测试')
