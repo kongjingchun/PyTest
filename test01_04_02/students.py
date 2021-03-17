7c25af3a9c65a3793c680bcf5f5530b8f4d6f4df
kongjingchun <jing20130808@vip.qq.com>
2021-03-17 18:59:25 +0800

    异常操作

--- test01_04_02/students.py
+++ test01_04_02/students.py
@@ -3,12 +3,16 @@
 # @Author: KongJingchun
 # @remark:实现学生信息库面向对象并增加批量功能
 
+class NotAgeError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
 class StudentInfo(object):
     def __init__(self, students):
         self.students = students
 
     def get_all(self):
-        print(len(students))
         for key, value in self.students.items():
             print(
                 '学号：{}、姓名：{}、年龄：{}、性别：{}、工作：{}'.format(key, value['name'], value['age'], value['sex'],
@@ -16,14 +20,26 @@ class StudentInfo(object):
 
     def check(self, **kwargs):
         if 'name' not in kwargs:
-            return '请输入正确的姓名'
+            raise NotAgeError('没有学生的的姓名')
         if 'age' not in kwargs:
-            return '请输入正确的年龄'
+            raise NotAgeError('没有学生的的年龄')
         if 'sex' not in kwargs:
-            return '请输入正确的性别'
+            raise NotAgeError('没有学生的的性别')
         if 'class_name' not in kwargs:
-            return '请输入正确的工作'
-        return True
+            raise NotAgeError('没有学生的的工作')
+        name_value = kwargs['name']
+        age_value = kwargs['age']
+        sex_value = kwargs['sex']
+        class_name_value = kwargs['class_name']
+
+        if not isinstance(name_value, str):
+            raise TypeError('请输入正确的姓名')
+        if not isinstance(age_value, int):
+            raise TypeError('请输入正确的年龄')
+        if not isinstance(sex_value, str):
+            raise TypeError('请输入正确的性别')
+        if not isinstance(class_name_value, str):
+            raise TypeError('请输入正确的姓名')
 
     def delete(self, id):
         if id in self.students:
@@ -35,20 +51,22 @@ class StudentInfo(object):
 
     def adds(self, students):
         for student in students:
-            message = self.check(**student)
-            if message != True:
-                print(message)
-            else:
-                self.__add(student)
+            try:
+                self.check(**student)
+            except Exception as e:
+                print(e)
+                continue
+            self.__add(student)
 
     def deletes(self, ids):
         for id_ in ids:
             self.delete(id_)
 
     def add_student(self, **student):
-        message = self.check(**student)
-        if message != True:
-            print(message)
+        try:
+            self.check(**student)
+        except Exception as e:
+            print(e)
         else:
             self.__add(student)
 
@@ -65,35 +83,36 @@ class StudentInfo(object):
         if kwargs['id'] not in self.students:
             print('请输入正确的id')
             return
-        message = self.check(**kwargs)
-        if message != True:
-            print(message)
-        else:
-            self.students[kwargs['id']] = {
-                'name': kwargs['name'],
-                'age': kwargs['age'],
-                'sex': kwargs['sex'],
-                'class_name': kwargs['class_name']
-            }
-            print('已更改学生信息为：学号：{}、姓名：{}、年龄：{}、性别：{}、工作：{}'.format(kwargs['id'], kwargs['name'], kwargs['age'],
-                                                                  kwargs['sex'],
-                                                                  kwargs['class_name']))
+        try:
+            self.check(**kwargs)
+        except Exception as e:
+            raise e
+        self.students[kwargs['id']] = kwargs
+        print('已更改学生信息为：学号：{}、姓名：{}、年龄：{}、性别：{}、工作：{}'.format(kwargs['id'], kwargs['name'], kwargs['age'],
+                                                              kwargs['sex'],
+                                                              kwargs['class_name']))
 
     def updates(self, update_students):
         for student in update_students:
-            id_ = list(student.keys())[0]
+            try:
+                id_ = list(student.keys())[0]
+            except IndexError as e:
+                print(e)
+                continue
             if id_ not in self.students:
                 print('ID: {} 不存在'.format(id_))
                 continue
             user_info = student.get(id_)
-            message = self.check(**user_info)
-            if message != True:
-                print(message)
+            try:
+                self.check(**user_info)
+            except Exception as e:
+                print(e)
                 continue
             self.students[id_] = user_info
             print('所有信息更新完成')
 
     def find_user(self, **kwargs):
+        assert len(kwargs) == 1, '参数数量传递错误'
         values = list(students.values())
         # key = None
         # value = None
@@ -108,7 +127,7 @@ class StudentInfo(object):
             key = 'class_name'
             value = kwargs
         else:
-            print('没有发现可搜索的关键字')
+            raise NotAgeError('没有发先可搜索的关键字')
             return
         for user in values:
             if value in user[key]:
@@ -151,21 +170,23 @@ if __name__ == '__main__':
     }
 
     student_info = StudentInfo(students)
-    student_info.get_all()
-    student_info.add_student(name='曹雅杰', age=27, sex='girl', class_name='家庭主妇')
-    student_info.delete(6)
+    # student_info.get_all()
+    # student_info.add_student(name='曹雅杰', age='12as 3', sex='girl', class_name='家庭主妇')
+    # student_info.get_all()
+    # student_info.delete(6)
     student_info.update(id=1, name='kjc', age=27, sex='boy', class_name='开发测试')
-    users = [
-        {'name': '批量添加1', 'age': 1, 'sex': 'boy', 'class_name': '批量测试'},
-        {'name': '批量添加2', 'age': 2, 'sex': 'boy', 'class_name': '批量测试'}
-    ]
-    student_info.adds(users)
     student_info.get_all()
-    student_info.deletes([1, 2])
-    student_info.updates([
-        {60: {'name': '批量添加10', "age": 10, 'sex': "boy1", 'class_name': '批量修改1'}},
-        {7: {'name': '批量添加20', "age": 20, 'sex': "boy2", 'class_name': '批量修改2'}},
-    ])
-    student_info.get_all()
-    st = student_info.find_user(name='倩')
-    print(st)
+    # users = [
+    #     {'name': '批量添加1', 'age': 1, 'sex': 'boy', 'class_name': '批量测试'},
+    #     {'name': '批量添加2', 'age': 2, 'sex': 'boy', 'class_name': '批量测试'}
+    # ]
+    # student_info.adds(users)
+    # student_info.get_all()
+    # student_info.deletes([1, 2])
+    # student_info.updates([
+    #     {60: {'name': '批量添加10', "age": 10, 'sex': "boy1", 'class_name': '批量修改1'}},
+    #     {7: {'name': '批量添加20', "age": 20, 'sex': "boy2", 'class_name': '批量修改2'}},
+    # ])
+    # student_info.get_all()
+    # st = student_info.find_user(name='倩')
+    # print(st)
---
+++ test01_05_01/__init__.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+# @Create time: 2021/3/17 6:59 下午
+# @Author: KongJingchun
+# @remark:
