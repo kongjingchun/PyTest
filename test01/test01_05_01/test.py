8e72fda56487924dc2698ff2a7ab71ad7dc6f872
kongjingchun <jing20130808@vip.qq.com>
2021-03-24 10:32:56 +0800

    目录调整

--- test01/test01_05_01/test.py
+++ test01/test01_05_01/test.py
@@ -2,7 +2,7 @@
 # @Create time: 2021/3/17 7:56 下午
 # @Author: KongJingchun
 # @remark:
-from test01_05_01 import cat,dog
+from test01.test01_05_01 import cat,dog
 
 result = cat.run()
 print(result)
---
+++ test01/test_01_06_01/__init__.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+# @Create time: 2021/3/24 10:31 上午
+# @Author: KongJingchun
+# @remark:
