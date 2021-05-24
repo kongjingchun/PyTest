0a9e7302938cda294fe17eca350c6ec5dc54452b
kongjingchun <jing20130808@vip.qq.com>
2021-05-25 00:25:30 +0800

    1

--- test.py
+++ test.py
@@ -3,3 +3,10 @@
 # @Author: KongJingchun
 # @remark:练习题
 
+import mysql.connector
+
+con = mysql.connector.connect(
+    host="49.233.5.13", prot="3306",
+    user="root", password="Jingchun0302!",
+    database="demo"
+)
---
+++ test01/test02_10_02/__init__.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+# @Create time: 2021/5/25 12:12 上午
+# @Author: KongJingchun
+# @remark:
